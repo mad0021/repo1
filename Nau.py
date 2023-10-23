74275d8d6a53745f18cfee32f9225b87d08dd9cf
mad0021 <123090580+mad0021@users.noreply.github.com>
2023-10-23 18:04:56 +0200

    afegit el fixter nau.py

---
+++ Nau.py
@@ -0,0 +1,37 @@
+def menu():
+    print("w- Moure amunt")
+    print("a- Moure equerre")
+    print("d- Moure amunt")
+    print("s- Moure avall")    
+    print("0- Sortir")
+
+def main():
+
+    menu()
+    
+    posX = 0
+    posY = 0
+
+    sortir=False
+    while not sortir:
+        op = input('Entra una opció: ')
+        if op=='d':
+            #sumar 1 a la variable posX
+            posX=posX+1
+        elif op=='a':
+            #restar 1 a la variable posX
+            posX=posX-1
+        elif op=='w':
+            #sumar 1 a la variable posY
+            posY=posY+1
+        elif op=='s':
+            #restar 1 a la variable posY
+            posY=posY-1
+        elif op=='0':
+            sortir=True
+            print("Has sortit de la nau")
+        
+        print(f"La nau està a la posició ({posX},{posY})")
+        
+if __name__ == "__main__":
+    main()
